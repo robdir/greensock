9d2ba7a4e98474637ec2d290e2f42714f9b41af2
robdir <rhwickham@gmail.com>
2018-02-07 14:50:22 +0100

    begin ad work

--- src/app.js
+++ src/app.js
@@ -15,6 +15,11 @@ $(document).ready(function() {
     TweenLite.to("#demo", 2, { backgroundColor: "#ff0000", width: "50%", top: "100px", ease: Power2.easeInOut });
 })
 
-$(document).ready(function() {
-    TweenLite.to('#logo', 2, { left: "650px", ease: Bounce.easeout });
-})
+window.onload = function() {
+    restartBtn = document.getElementById("restartBtn"),
+    tween = TweenLite.to('#logo', 1, { left: "440px" });
+
+    restartBtn.onclick = function() {
+        tween.restart();
+    }
+}
