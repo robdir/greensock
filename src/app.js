867a96572fb3fdd9c1fa3e224abfe0fcc0e12db6
robdir <rhwickham@gmail.com>
2018-02-07 16:21:29 +0100

    rotation complete

--- src/app.js
+++ src/app.js
@@ -11,21 +11,24 @@
 // })
 
 // shorthand!! 
-$(document).ready(function() {
-    TweenLite.to("#demo", 2, { backgroundColor: "#ff0000", width: "50%", top: "100px", ease: Power2.easeInOut });
-})
+// $(document).ready(function() {
+//     TweenLite.to("#demo", 2, { backgroundColor: "#ff0000", width: "50%", top: "100px", ease: Power2.easeInOut });
+// })
 
-window.onload = function() {
-    restartBtn = document.getElementById("restartBtn"),
-    tween = TweenLite.to('#logo', 1, { left: "440px" });
+// window.onload = function() {
+//     restartBtn = document.getElementById("restartBtn"),
+//     tween = TweenLite.to('#logo', 1, { left: "440px" });
 
-    restartBtn.onclick = function() {
-        tween.restart();
-    }
-}
+//     restartBtn.onclick = function() {
+//         tween.restart();
+//     }
+// }
+
 
 window.onload = function() {
 animate = document.getElementById("animate")
 animate.onclick = function() {
+    var sun = document.getElementById("adsun")
+    TweenLite.to(sun, 2, {directionalRotation:"360_cw"})
     }
 }
