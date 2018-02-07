53083c1bad246a80fb155aa60c40f15fd9f68480
robdir <rhwickham@gmail.com>
2018-02-07 13:39:17 +0100

    easing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
+// $(document).ready(function() {
+//     var logo = $('#logo')
+//     TweenLite.to(logo, 1, { left: "632px" });
+// })
+
+// $(document).ready(function () {
+//     var div = $('#demo')
+//     div.on('click', function() {
+//         TweenLite.to(demo, 1, { left: "632px" });
+//     })
+// })
+
+// shorthand!! 
 $(document).ready(function() {
-    var logo = $('#id')
-    logo.on('click', function(){
-        $(this).animate({ 'top': '-14px', 'opacity': '1' }, 'fast');
-    })
-    
+    TweenLite.to("#demo", 2, { backgroundColor: "#ff0000", width: "50%", top: "100px", ease: Power2.easeInOut });
+})
+
+$(document).ready(function() {
+    TweenLite.to('#logo', 2, { left: "650px", ease: Bounce.easeout });
 })
